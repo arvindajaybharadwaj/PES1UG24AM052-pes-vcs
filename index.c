c54300c3e8ec1486987fba832423e1aaed3aa1de
arvindajaybharadwaj <arvindajayarts@gmail.com>
2026-04-21 11:16:32 +0530

    implement index_save

--- index.c
+++ index.c
@@ -207,10 +207,27 @@ int index_load(Index *index)
 // Returns 0 on success, -1 on error.
 int index_save(const Index *index)
 {
-    // TODO: Implement atomic index saving
-    // (See Lab Appendix for logical steps)
-    (void)index;
-    return -1;
+    FILE *f = fopen(INDEX_FILE ".tmp", "w");
+    if (!f)
+        return -1;
+
+    for (int i = 0; i < index->count; i++)
+    {
+        char hex[HASH_HEX_SIZE + 1];
+        hash_to_hex(&index->entries[i].hash, hex);
+
+        fprintf(f, "%o %s %lu %u %s\n", index->entries[i].mode, hex, index->entries[i].mtime_sec, index->entries[i].size, index->entries[i].path);
+
+        printf("SAVE: %s\n", index->entries[i].path);
+    }
+
+    fflush(f);
+    fsync(fileno(f));
+    fclose(f);
+
+    rename(INDEX_FILE ".tmp", INDEX_FILE);
+
+    return 0;
 }
 
 // Stage a file for the next commit.
